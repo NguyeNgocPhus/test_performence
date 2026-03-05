3cea69d876f17a88f106a365e0b2cf6dbeced095
Phus <phuteobs0166@gmail.com>
2026-03-05 20:53:40 +0700

    fix

--- TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cfefa71eb42b22918b7a6f0358ec1d303974183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336b8e2b86b0b66b4d81b0dea22ab4de2574582b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
